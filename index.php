3033efbe2ac71bde8a6904ed0c57b158d63a4577
numerotres865 <rubentgonzalez@gmail.com>
2023-03-22 01:25:06 -0400

    haha uwu

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+#imprimision
+echo '<h1> Hola mundo</h1>';
+
+print('HOLAS <br>');
+$x = 5;
+$y = 4;
+$z = $x+$y;
+echo $z;
+
+#Variables 
+echo "<h1> La suma de ".$x." y ".$y." es: ".$z."</h1>";
+
+?>
